8f0f4077d9cdc4b5246babc76a61cbc4098bdc88
Crystal Root <crystal.l.root@gmail.com>
2021-09-14 12:06:03 -0700

    turned into function component template functioning

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,37 +1,110 @@
 import React, { Component } from 'react';
-import { Button, Label, Col, Row, Form, FormGroup, Input } from 'reactstrap';
+// import { Button, Form, FormGroup, Label, Input, Col, FormFeedback } from 'reactstrap';
+// import { Link } from 'react-router-dom';
 import '../App.css';
 
-class FormComponent extends Component {
-    constructor(props) {
-        super(props);
+const RadioInput = ({label, value, checked, setter}) => {
+	return (
+        <label>
+            <input type="radio" checked={checked == value}
+                onChange={() => setter(value)} />
+            <span>{label}</span>
+        </label>
+	);
+};
 
-        this.state = {value: ''};
+const Form = props => {
+	const [dose1, setDose1] = React.useState();
+	const [dose2, setDose2] = React.useState();
+	const handleSubmit = e => {
+		e.preventDefault();
+		const data = {dose1, dose2};
+		const json = JSON.stringify(data, null, 4);
+		console.clear();
+		console.log(json);
+        alert(json);
+	};
+	return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Dose 1:</label>
+                <RadioInput label="2.5 mg" value="2.5 mg" checked={dose1} setter={setDose1}  />
+                <RadioInput label="5 mg" value="5 mg" checked={dose1} setter={setDose1} />
+            </div>
+            <div>
+                <label>Dose 2:</label>
+                <RadioInput label="2.5 mg" value="2.5 mg" checked={dose2} setter={setDose2} />
+                <RadioInput label="5 mg" value="5 mg" checked={dose2} setter={setDose2}  />
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+	);
+};
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+// class FormComponent extends Component {
+//     constructor(props) {
+//         super(props);
 
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
+//         this.state = {
+//             dose1: '',
+//             dose2: ''
+//         };
 
-    handleSubmit(event) {
-        alert('Current state is: ' + this.state.value);
-        event.preventDefault();
-    }
+//         this.handleInputChange = this.handleInputChange.bind(this);
+//         this.handleSubmit = this.handleSubmit.bind(this);
+//     }
 
-    render() {
+//     handleInputChange(event) {
+//         const target = event.target;
+//         const value = target.type === 'radio' ? target.checked : target.value;
+//         const name = target.name;
 
-        return (
-            <Form onSubmit={this.handleSubmit}>
-                <Label htmlFor="dose1">Dose 1:</Label>
-                    <Input type="radio" value="2.5 mg" onChange={this.handleChange} name="dose1" /> 2.5 mg
-                    <Input type="radio" value="5 mg" onChange={this.handleChange} name="dose1" />5 mg
-                    <Input type="submit" value="Submit" />
-            </Form>
-        );
-    }
-}
+//         this.setState({
+//             [name]: value
+//         });
+//     }
 
-export default FormComponent;
+//     handleSubmit(values) {
+//         alert('Current state is: ' + JSON.stringify(this.state));
+//     }
+
+//     render() {
+
+//         return (
+//             <div className="container">
+//                 <h5>Today's Doses</h5>
+//                 <Form onSubmit={values => this.handleSubmit}>
+//                     <div className="radio">
+//                         <div className="row">Dose 1:
+//                         <Label>
+//                             <Input 
+//                                 type="radio"
+//                                 name="dose1"
+//                                 value="2.5mg"
+//                                 onChange={this.handleInputChange}> 2.5 mg
+//                             </Input>
+//                         </Label>
+//                         <Label>
+//                             <Input 
+//                                 type="radio"
+//                                 name="dose1"
+//                                 value="5 mg"
+//                                 onChange={this.handleInputChange}> 5 mg
+//                             </Input>
+//                         </Label>
+//                     </div>
+//                     </div>
+//                     <div className="row">
+//                         <Label htmlFor="dose1">Dose 2:</Label>
+//                             <Input type="radio" value="2.5 mg" onChange={this.handleInputChange} name="dose2" /> 2.5 mg
+//                             <Input type="radio" value="5 mg" onChange={this.handleInputChange} name="dose2" />5 mg
+//                     </div>
+
+//                             <Input type="submit" value="Submit" />
+//                 </Form>
+//             </div>
+//         );
+//     }
+// }
+
+export default Form;
